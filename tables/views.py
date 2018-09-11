b56933371636174e4c4495be79dec1d393b5ddb6
Garrett Graham <gwgraham@uark.edu>
2018-09-10 21:53:09 -0500

    beginning API V2 migrations

--- tables/views.py
+++ tables/views.py
@@ -13,56 +13,56 @@ def get_data(url,numTeams, request):
     # team names list
     team_names = []
     for i in range(numTeams):
-        team_names.append(leaguedata['standing'][i]['teamName'])
+        team_names.append(leaguedata['standings'][0]['table'][i]['team']['name'])
     # team rankings list
     team_posits = []
     for i in range(numTeams):
-        team_posits.append(leaguedata['standing'][i]['position'])
+        team_posits.append(leaguedata['standings'][0]['table'][i]['position'])
     # team matches played list
     team_mp = []
     for i in range(numTeams):
-        team_mp.append(leaguedata['standing'][i]['playedGames'])
+        team_mp.append(leaguedata['standings'][0]['table'][i]['playedGames'])
     # team wins list
     team_wins = []
     for i in range(numTeams):
-        team_wins.append(leaguedata['standing'][i]['wins'])
+        team_wins.append(leaguedata['standings'][0]['table'][i]['won'])
     # team draws list
     team_draw = []
     for i in range(numTeams):
-        team_draw.append(leaguedata['standing'][i]['draws'])
+        team_draw.append(leaguedata['standings'][0]['table'][i]['draw'])
     # team losses list
     team_loss = []
     for i in range(numTeams):
-        team_loss.append(leaguedata['standing'][i]['losses'])
+        team_loss.append(leaguedata['standings'][0]['table'][i]['lost'])
     # team goals for list
     team_gf = []
     for i in range(numTeams):
-        team_gf.append(leaguedata['standing'][i]['goals'])
+        team_gf.append(leaguedata['standings'][0]['table'][i]['goalsFor'])
     # team goals against list
     team_ga = []
     for i in range(numTeams):
-        team_ga.append(leaguedata['standing'][i]['goalsAgainst'])
+        team_ga.append(leaguedata['standings'][0]['table'][i]['goalsAgainst'])
     # team goal differential list
     team_gd = []
     for i in range(numTeams):
-        team_gd.append(leaguedata['standing'][i]['goalDifference'])
+        team_gd.append(leaguedata['standings'][0]['table'][i]['goalDifference'])
     # team points list
     team_points = []
     for i in range(numTeams):
-        team_points.append(leaguedata['standing'][i]['points'])
+        team_points.append(leaguedata['standings'][0]['table'][i]['points'])
 
     table = []
     for N, P, M, W, D, L, GF, GA, GD, PI in zip(team_names, team_posits, team_mp, team_wins, team_draw, team_loss, team_gf, team_ga, team_gd, team_points):
         table.append([N, P, M, W, D, L, GF, GA, GD, PI])
 
     return render(request, 'tables/league_table.html', {
-        'matchday' : leaguedata['matchday'],
-        'leagueCaption' : leaguedata['leagueCaption'],
+        'matchday' : leaguedata['season']['currentMatchday'],
+        'leagueCaption' : leaguedata['competition']['name'],
         'table' : table
     })
 
 def bundesliga_1(request):
-    league_url = 'http://api.football-data.org/v1/competitions/452/leagueTable'
+    league_url = 'http://api.football-data.org/v2/competitions/2002/standings'
     return get_data(league_url, 18, request)
 
 def premier_league(request):
